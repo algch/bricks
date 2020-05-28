45757651a5be7a91a5f5f659db8d8feeb3b1b0f2
Edgar en hexaedro <drarbego@gmail.com>
2020-05-28 01:26:47 -0500

    more improvements to arrow mirroring

--- weapons/arrow.gd
+++ weapons/arrow.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var direction = Vector2(0, 0)
-var speed = 400
+var speed = 100
 onready var player = get_node('/root/arena/player')
 
 func _ready():
@@ -37,8 +37,9 @@ func _physics_process(delta):
 	if collision:
 		self.handleCollision(collision)
 
-remote func updateArrow(pos):
+remote func updateArrow(pos, dir):
 	position = pos
+	direction = dir
 
 func _on_updateTimer_timeout():
 	if get_tree().is_network_server():
@@ -47,5 +48,6 @@ func _on_updateTimer_timeout():
 		var mirrored_x = X + (X - position.x)
 		var mirrored_y = Y + (Y - position.y)
 		var mirrored_pos = Vector2(mirrored_x, mirrored_y)
-		rpc('updateArrow', mirrored_pos)
+		var mirrored_dir = direction.rotated(PI)
+		rpc('updateArrow', mirrored_pos, mirrored_dir)
 		$updateTimer.start()
